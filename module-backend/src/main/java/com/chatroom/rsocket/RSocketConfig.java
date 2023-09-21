5a096821e02b09c10a8a6534bcd931d19be7f147
sehee gwon <gwonsehwi@gmail.com>
2023-09-21 23:50:30 +0900

    feature/ISSUE-1 RSocket 기본 설정, Connection 기능 추가

---
+++ module-backend/src/main/java/com/chatroom/rsocket/RSocketConfig.java
@@ -0,0 +1,29 @@
+package com.chatroom.rsocket;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.cbor.Jackson2CborDecoder;
+import org.springframework.http.codec.cbor.Jackson2CborEncoder;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.web.util.pattern.PathPatternRouteMatcher;
+
+@Configuration
+public class RSocketConfig {
+
+    @Bean
+    public RSocketMessageHandler rsocketMessageHandler() {
+        RSocketMessageHandler handler = new RSocketMessageHandler();
+        handler.setRSocketStrategies(rSocketStrategies());
+        return handler;
+    }
+
+    @Bean
+    public RSocketStrategies rSocketStrategies() {
+        return RSocketStrategies.builder()
+                .encoders(encoders -> encoders.add(new Jackson2CborEncoder()))
+                .decoders(decoders -> decoders.add(new Jackson2CborDecoder()))
+                .routeMatcher(new PathPatternRouteMatcher())
+                .build();
+    }
+}
